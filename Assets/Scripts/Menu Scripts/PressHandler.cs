2b6c2030a5b3503f958d27cbf4979c00175c84fa
xwing027 <seventyseventeen@gmail.com>
2021-11-13 15:40:28 +1100

    main menu 1

--- Assets/Scripts/Menu Scripts/PressHandler.cs
+++ Assets/Scripts/Menu Scripts/PressHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class PressHandler : MonoBehaviour
 {
@@ -13,6 +14,8 @@ public class PressHandler : MonoBehaviour
         {
             mainMenu.gameObject.SetActive(true);
             gameObject.SetActive(false);
+
+            //play animations
         }
     }
 }
